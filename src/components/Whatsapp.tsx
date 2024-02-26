43dc5482c31e98af9e9443576ac88cc2c12e079e
Jasurbek Xakimbekov <92104896+Jasurbek0424@users.noreply.github.com>
2024-02-26 10:55:19 +0500

    Update Whatsapp.tsx

--- src/components/Whatsapp.tsx
+++ src/components/Whatsapp.tsx
@@ -1,24 +1,19 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { FaWhatsapp } from 'react-icons/fa6';
 
 const Whatsapp: React.FC = () => {
-    const [showButton, setShowButton] = useState(false);
 
-    useEffect(() => {
-        setShowButton(true);
-    }, []);
 
 
     return (
         <div>
-            {showButton && (
-                <a href='https://wa.me/+77007192085' className="scroll-to-top 2xl:text-5xl xl:text-4xl lg:text-3xl
+
+            <a href='https://wa.me/+77007192085' className="scroll-to-top 2xl:text-5xl xl:text-4xl lg:text-3xl
                  md:text-2xl sm:text-xl text-xl shadow-emerald-50 shadow
-                fixed bottom-5 left-5 p-2 bg-green-400 text-white cursor-pointer rounded z-30"
-                >
-                    <FaWhatsapp />
-                </a>
-            )}
+                fixed bottom-5 left-5 p-2 bg-green-500 text-white cursor-pointer rounded z-30"
+            >
+                <FaWhatsapp />
+            </a>
         </div>
     );
 };
